b4d9a39f96ce4103401c752ad7d39ed849626239
Saravanansj <146342351+Saravanansj@users.noreply.github.com>
2025-08-27 16:46:49 +0530

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -2,44 +2,46 @@ package org.saro;
 
 public class Teacher {
 
+    private int id;
+    private String name;
+    private String position;
+    private long phone;
 
-		// TODO Auto-generated method stub
-		private int id;
-		private String name;
-		private String Position;
-		private long phone;
-		public int getId()
-		{
-			return id;
-		}
-		public void setId(int id)
-		{
-			this.id=id;
-		}
-		public String getName()
-		{
-			return name;
-		}
-		public void setName(String name)
-		{
-			this.name=name;
-		}
-		public String getPosition()
-		{
-			return Position;
-		}
-		public void setPosition(String Position)
-		{
-			this.Position=Position;
-		}
-		public long getPhone()
-		{
-			return phone;
-		}
-		public void setPh(long phone)
-		{
-			this.phone=phone;
-		}
-		
+    // Getter & Setter for id
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    // Getter & Setter for name
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter & Setter for position
+    public String getPosition() {
+        return position;
+    }
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    // Getter & Setter for phone
+    public long getPhone() {
+        return phone;
+    }
+    public void setPh(long phone) {
+        this.phone = phone;
+    }
+
+    // Optional: toString for easy display
+    @Override
+    public String toString() {
+        return "Teacher [ID=" + id + ", Name=" + name + ", Position=" + position + ", Phone=" + phone + "]";
+    }
 }
