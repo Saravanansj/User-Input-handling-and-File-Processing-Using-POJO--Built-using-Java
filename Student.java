45ea6f5865b6c9c57d414ff67821b504679fb49a
Saravanansj <146342351+Saravanansj@users.noreply.github.com>
2025-08-27 16:46:17 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -2,33 +2,37 @@ package org.saro;
 
 public class Student {
 
-	private int id;
-	private String name;
-	private long phone;
-	public int getId()
-	{
-		return id;
-	}
-	public void setId(int id)
-	{
-		this.id=id;
-	}
-	public String getName()
-	{
-		return name;
-	}
-	public void setName(String name)
-	{
-		this.name=name;
-	}
-	public long getPhone()
-	{
-		return phone;
-	}
-	public void setPh(long phone)
-	{
-		this.phone=phone;
-	}
-	
+    private int id;
+    private String name;
+    private long phone;
 
+    // Getter & Setter for id
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Getter & Setter for name
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter & Setter for phone
+    public long getPhone() {
+        return phone;
+    }
+    public void setPh(long phone) {
+        this.phone = phone;
+    }
+
+    // Optional: toString for easy display
+    @Override
+    public String toString() {
+        return "Student [ID=" + id + ", Name=" + name + ", Phone=" + phone + "]";
+    }
 }
